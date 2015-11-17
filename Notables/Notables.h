bbbf49a45e5cbf174496b9ac612520e3e5e94457
Josh Holtz <josh@rokkincat.com>
2015-11-17 17:03:34 -0600

    Added umbrella header guy

---
+++ Notables/Notables.h
@@ -0,0 +1,14 @@
+//
+//  Notables.h
+//  Notables
+//
+//  Created by Josh Holtz on 11/17/15.
+//  Copyright © 2015 RokkinCat. All rights reserved.
+//
+
+#ifndef Notables_h
+#define Notables_h
+
+@import Notables
+
+#endif /* Notables_h */
