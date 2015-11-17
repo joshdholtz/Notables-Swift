9f892be08bb6870391e2fc8c34b42159f9ae606b
Josh Holtz <josh@rokkincat.com>
2015-11-17 16:04:54 -0600

    Added framework

---
+++ NotablesFramework/NotablesFramework.h
@@ -0,0 +1,19 @@
+//
+//  NotablesFramework.h
+//  NotablesFramework
+//
+//  Created by Josh Holtz on 11/17/15.
+//  Copyright © 2015 RokkinCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NotablesFramework.
+FOUNDATION_EXPORT double NotablesFrameworkVersionNumber;
+
+//! Project version string for NotablesFramework.
+FOUNDATION_EXPORT const unsigned char NotablesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotablesFramework/PublicHeader.h>
+
+
